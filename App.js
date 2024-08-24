1c0540cd274a7885a8eb693d7d85b53541f54a92
harshasanapu100 <harshasanapu100@gmail.com>
2024-08-24 10:39:17 +0530

    Add parcel, react and react-dom package

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./App.css";
+
 const span = React.createElement("span", { key: "s1" }, "First Span");
 const paragrpah = React.createElement("p", { key: "p1" }, "First Paragraph");
 const container = React.createElement("div", { className: "container" }, [
